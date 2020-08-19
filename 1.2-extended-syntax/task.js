c882c0dd35f6f2cf98835390c09ab9bda4e90db7
Margarita Sharova <margaritasharova@MacBook-Air-Margarita.local>
2020-08-19 15:05:41 +0400

    incorrect variant

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,6 +1,45 @@
+"use strict";
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+
+    a = 2;
+    b = 4;
+    c = 3; 
+
+    let d = (b**2) + (4 * a * c);
+   // Math.sqrt(expr);
+
+    let result = [];
+    let rootFirst;
+    let rootSecond
+    
+    if (d < 0) {
+        result = [];
+    } else if (d == 0) {
+        rootFirst = (-b + Math.sqrt(d)) / 2*a;
+        result.push(rootFirst);
+        
+    } else {
+       // result[0] = (-b + Math.sqrt(d)) / 2*a;
+        //result[1] = (-b - Math.sqrt(d)) / 2*a;
+
+        rootFirst = (-b + Math.sqrt(d)) / 2*a;
+        rootSecond = (-b - Math.sqrt(d)) / 2*a;
+        result.push(rootFirst, rootSecond);
+    }
+    return result;
+
+
+   /* if ( d == 0) {
+        result[0] = (-b + Math.sqrt(d)) / 2*a;
+    } else if (d > 0) {
+        result[0] = (-b + Math.sqrt(d)) / 2*a;
+        result[1] = (-b - Math.sqrt(d)) / 2*a;
+    } else {
+        result = null;
+    }
+    return result; */
+
 }
 
 function getAverageMark(marks){
