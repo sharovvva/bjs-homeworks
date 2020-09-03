4826592f6ff413b28e635d97d8bff83ddf9de5e4
Margarita Sharova <margaritasharova@MacBook-Air-Margarita.local>
2020-09-03 19:58:26 +0400

    hw

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,51 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function () {
+    let string = this.toLowerCase();
+    //let revStr= this.toLowerCase().split('').reverse().join('');
+    //this.string = string;
+    //this.revStr = revStr;
+    /*if (this.string === this.revStr.split('').reverse().join('')) {
+        return true;
+    } else {
+        return false;
+    }  */
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === string[string.length - 1 -i]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+console.log('А роза упала на лапу Азора');
+
+//const palindrom = new isPalindrom('А роза упала на лапу Азора');
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let result;
+    let average;
+    if (marks != 0) {
+        result = marks.reduce(function(sum, current) {
+            return sum + current;
+        }, 0);
+        average = result / marks.length;
+        let roundedAverage = Math.round(average);
+        return roundedAverage
+    } else {
+        return 0
+    }
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Date.now();
+    let bday = new Date(birthday);
+    birthday = +bday;
+    let diff = now - birthday;
+    let age = diff / 31536000000;
+    if (age > 18) {
+        return true;
+    } else {
+        return false;
+    } 
 }
